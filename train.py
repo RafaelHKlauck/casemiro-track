80cf53eea7b1a27e3ad49b2effd287c7154d3721
Rafael Hansen Klauck <rhansenklauck@gmail.com>
2026-04-20 20:44:40 -0300

    Update training parameters and refine dependencies
    - Increased training epochs from 10 to 20 in `train.py` and added recommended augmentations for improved model performance.
    - Updated `.gitignore` to remove specific `min` directories.
    - Expanded `requirements.txt` to include a comprehensive list of dependencies for the project.

--- train.py
+++ train.py
@@ -5,9 +5,25 @@ if __name__ == "__main__":
 
     results = model.train(
         data="data.yaml",
-        epochs=10,
+        epochs=20,
         imgsz=640,
         batch=16,
         name="casemiro-train",
-        exist_ok=True
+        exist_ok=True,
+
+        # 🔄 Augmentations recomendadas
+        fliplr=0.5,     # flip horizontal (importante)
+        flipud=0.0,     # NÃO usar vertical
+
+        scale=0.5,      # zoom (muito importante)
+        translate=0.1,  # leve movimento da imagem
+
+        hsv_h=0.015,    # cor (leve)
+        hsv_s=0.7,      # saturação
+        hsv_v=0.4,      # brilho (importante)
+
+        mosaic=1.0,     # forte generalização
+        mixup=0.15,     # leve mistura de imagens
+
+        degrees=0.0,    # sem rotação (ou bem baixo)
     )
